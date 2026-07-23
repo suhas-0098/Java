a5a123b9cc76c4695146dad3b7e7a484b23d0926
suhas-0098 <190030098cse@gmail.com>
2026-07-23 10:42:59 +0530

    classes and objects demo

---
+++ 01-First-Java/src/classes/Car.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Car {
+    String brand;
+    String color;
+    int speed;
+
+    void drive(){
+        System.out.println(brand +" " + color + " is driving at speed " + speed);
+    }
+
+}
---
+++ 01-First-Java/src/classes/ClassesObjectsDemo.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class ClassesObjectsDemo {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        car1.brand ="Toyota";
+        car1.speed = 100;
+        car1.color = "red";
+        car1.drive();
+
+        Car car2 = new Car();
+        car2.brand ="BMW";
+        car2.speed = 140;
+        car2.color = "Blue";
+        car2.drive();
+
+
+
+    }
+}
